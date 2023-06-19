65c7571c7a0e1b8e6ee8f20355a9b39c40478e32
WillOli <william.ika@gmail.com>
2023-06-19 16:05:58 -0300

    Entity User e Rest funcionando

---
+++ src/main/java/com/willoli/workshopmongo/domain/User.java
@@ -0,0 +1,69 @@
+package com.willoli.workshopmongo.domain;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+
+
+    private String id;
+    private String name;
+    private String email;
+
+    /*Construtor vazio*/
+    public User() {
+
+    }
+
+    /*Construtores com os atributos*/
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    /*Métodos de acesso Getters e Setter*/
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getemail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /*hashCode() and enquals()*/
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
+
