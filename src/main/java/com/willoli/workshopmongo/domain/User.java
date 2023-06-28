5bcb2f140a5229574a6d002d7f7c1e8c55fb1b7c
WillOli <william.ika@gmail.com>
2023-06-28 08:28:49 -0300

    Referenciado os posts do usuario

--- src/main/java/com/willoli/workshopmongo/domain/User.java
+++ src/main/java/com/willoli/workshopmongo/domain/User.java
@@ -2,8 +2,11 @@ package com.willoli.workshopmongo.domain;
 
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -16,6 +19,9 @@ public class User implements Serializable {
     private String id;
     private String name;
     private String email;
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
     /*Construtor vazio*/
     public User() {
@@ -50,6 +56,13 @@ public class User implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+    public void setPosts() {
+        this.posts = posts;
     }
 
     /*hashCode() and enquals()*/
