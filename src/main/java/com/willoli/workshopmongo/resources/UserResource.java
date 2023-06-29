5f5781d263ddb0b53847ec5bd34f2ab1dbedeab7
WillOli <william.ika@gmail.com>
2023-06-29 08:18:40 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/willoli/workshopmongo/resources/UserResource.java
+++ src/main/java/com/willoli/workshopmongo/resources/UserResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.willoli.workshopmongo.domain.Post;
 import com.willoli.workshopmongo.domain.User;
 import com.willoli.workshopmongo.dto.UserDTO;
 import com.willoli.workshopmongo.services.UserService;
@@ -56,6 +57,12 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 }
