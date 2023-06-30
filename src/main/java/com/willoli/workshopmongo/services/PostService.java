946f5df836640ff4f54eda04e332c903a3aeba52
WillOli <william.ika@gmail.com>
2023-06-30 08:04:55 -0300

    Obtendo post por id

---
+++ src/main/java/com/willoli/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.willoli.workshopmongo.services;
+import com.willoli.workshopmongo.Repository.PostRepository;
+import com.willoli.workshopmongo.domain.Post;
+import com.willoli.workshopmongo.services.excepction.ObjectFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post>obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectFoundException("Objeto não encontrado"));
+	}
+
+
+}
