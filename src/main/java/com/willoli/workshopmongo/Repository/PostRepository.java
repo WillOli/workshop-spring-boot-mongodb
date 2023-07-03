a8128a7ff60c32fc6b84c981f13a273e41204033
WillOli <william.ika@gmail.com>
2023-07-03 15:41:48 -0300

    Consulta simples com @Query

--- src/main/java/com/willoli/workshopmongo/Repository/PostRepository.java
+++ src/main/java/com/willoli/workshopmongo/Repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.willoli.workshopmongo.Repository;
 
 import com.willoli.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.w3c.dom.stylesheets.LinkStyle;
 
@@ -9,6 +10,8 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTititle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 
 
